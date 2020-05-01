dbc96ac81134836c528d02c3f1e3691d496bfbf7
Axel Obscura <axosar@gmail.com>
2020-05-01 15:39:45 -0500

    Second test

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from '../App';
+import CommentBox from '../CommentBox';
+import CommentList from '../CommentList';
 
 it("shows a comment box", () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-    expect(div.innerHTML).toContain('Comment BOX');
-    ReactDOM.unmountComponentAtNode(div);
+    const wrapped = shallow(<App />);
+    expect(wrapped.find(CommentBox).length).toEqual(1);
+})
+
+it("shows a comment list", () => {
+    const wrapped = shallow(<App />);
+    expect(wrapped.find(CommentList).length).toEqual(1);
 })
