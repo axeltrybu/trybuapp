451edddf71ccd548160ffda245820a0b6cf7d374
Axel Obscura <axosar@gmail.com>
2020-05-01 15:11:48 -0500

    Enzyme setup

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,4 +1,10 @@
 import React from 'react';
-import reactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
 import App from '../App';
 
+it("shows a comment box", () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    expect(div.innerHTML).toContain('Comment BOX');
+    ReactDOM.unmountComponentAtNode(div);
+})
