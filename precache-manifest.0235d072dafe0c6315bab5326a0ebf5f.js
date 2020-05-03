e7c32df2b03b459def6f1a4fb38fa565b47444d6
Axel Obscura <axosar@gmail.com>
2020-05-03 16:34:03 -0500

    Updates

---
+++ precache-manifest.0235d072dafe0c6315bab5326a0ebf5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d3f673cd65ff06805c6a2b7649632ed",
+    "url": "/trybuapp/index.html"
+  },
+  {
+    "revision": "2441486aaac6a1be132b",
+    "url": "/trybuapp/static/js/2.3604bf5b.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/trybuapp/static/js/2.3604bf5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "683a2fe51902e5d94158",
+    "url": "/trybuapp/static/js/main.8fc0abb6.chunk.js"
+  },
+  {
+    "revision": "6176891105f40a799da2",
+    "url": "/trybuapp/static/js/runtime-main.3087850e.js"
+  }
+]);
