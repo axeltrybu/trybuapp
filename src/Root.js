94db9e170d2b6a558bba04a882ad618a30ac856c
Axel Obscura <axosar@gmail.com>
2020-05-02 19:45:34 -0500

    Fetch comment redux and axios

--- src/Root.js
+++ src/Root.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { reduxPromise, applyMiddleware } from 'redux-promise';
 import reducers from 'reducers';
 
 export default ({ children, initialState = {} }) => {
+    const store = createStore(reducers, initialState, applyMiddleware(reduxPromise));
     return (
-        <Provider store={createStore(reducers, initialState)}>
+        <Provider store={store}>
             {children}
         </Provider>
     )
