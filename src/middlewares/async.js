50cb1fa02aa8ba3e7fef7546bb6e6cb0a8c00173
Axel Obscura <axosar@gmail.com>
2020-05-04 18:51:07 -0500

    Middleware cheking for action

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,4 +1,7 @@
 export default ({ dispatch }) => next => action => {
-
+    //if the action has a promise
+    if (!action.payload || !action.payload.then) {
+        return next(action);
+    }
 }
 
